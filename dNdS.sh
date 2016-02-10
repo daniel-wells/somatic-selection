bef06504d7f676f1452e9b095cc3a3269faacd8a
daniel-wells <daniel-wells@users.noreply.github.com>
2016-02-10 19:57:49 +0000

    remove python script to calculate non synonymous sites
    replaced with R script to calculate based on mutation profile

--- dNdS.sh
+++ dNdS.sh
@@ -26,17 +26,8 @@ sha256sum data/raw/mart_export.txt.gz >> data/raw/SHA256SUMS
 
 gzip -dc data/raw/Homo_sapiens.GRCh37.75.cds.all.fa.gz > data/Homo_sapiens.GRCh37.75.cds.all.fa
 
-python calculate_nonsynonymous_sites.py
-# 4min 30seconds for all genes
-
-# Remove tanscripts with errors (N as nucleotide or not multiple of 3) & non protein coding
-grep -Pv 'coding\tNA' nonsynonymous_sites_per_transcript.tsv | fgrep 'gene_biotype:protein_coding transcript_biotype:protein_coding' > nonsynonymous_sites_per_transcript_clean.tsv
-
-# remove variable names from every line
-sed -i 's/[[:space:]]transcript_biotype:/\t/g;s/[[:space:]]gene_biotype:/\t/g;s/[[:space:]]gene:/\t/g;s/[[:space:]]chromosome:/\t/g;s/[[:space:]]supercontig:/\t/g;s/[[:space:]]cds:/\t/g;s/:/\t/g' nonsynonymous_sites_per_transcript_clean.tsv
-
-# add variable names to heading
-sed -i '1s/^/transcript\tcds\tgenome\tchromosome\tstart\tend\tstrand\tgene\tgene_biotype\ttranscript_biotype\tnonsynonymous_sites\tcds_length\terror\n/' nonsynonymous_sites_per_transcript_clean.tsv
+Rscript calculate_nonsynon_sites.R
+# 15 mins
 
 ###############################################################################
 ####### Calculate (non)synonymous mutations by transcript from ICGC VCF #######
