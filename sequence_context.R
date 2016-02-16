119c817f9145e9c8f642185a742287b861d84dfe
daniel-wells <daniel-wells@users.noreply.github.com>
2016-02-16 19:54:24 +0000

    save a cache of all mutations rds
    previously all mutation files were loaded and immediately filtered
    (single base mutations & only exonic), but would be useful to be able
    to quickly load the entire set pre-processing for future tasks.

--- sequence_context.R
+++ sequence_context.R
@@ -23,16 +23,26 @@ if (file.exists("data/single.base.coding.substitutions.rds")){
 	read.and.filter <- function(x) {
 	print(paste("Reading file:",x))
 	data <- fread(x)
-	# all single base pair, exonic mutations
-	data <- data[mutation_type=="single base substitution" & consequence_type %in% c("missense_variant", "synonymous_variant", "frameshift_variant","disruptive_inframe_deletion","disruptive_inframe_insertion","inframe_deletion","inframe_insertion","start_lost","stop_lost","stop_gained")]
+	# all mutations, remove unused columns (~15% original size)
+	data <-  data[,.(icgc_mutation_id,icgc_donor_id,project_code,chromosome,chromosome_start,chromosome_end,chromosome_strand,mutation_type,reference_genome_allele,mutated_from_allele,mutated_to_allele,consequence_type,aa_mutation,cds_mutation,gene_affected,transcript_affected,sequencing_strategy)]
 	return(data)
 	}
 
 	# Load tsv mutations
-	single.base.coding.substitutions <- rbindlist(lapply(paste('zcat < ',file_list), read.and.filter))
+	mutations <- rbindlist(lapply(paste('zcat < ',file_list), read.and.filter))
 
-	# Save object
-	saveRDS(single.base.coding.substitutions, "data/single.base.coding.substitutions.rds")
+	# Set catagoricals as factors (saves 4GB)
+	mutations[, c("project_code","chromosome","chromosome_strand","mutation_type","consequence_type","sequencing_strategy","gene_affected","transcript_affected","icgc_donor_id") := lapply(mutations[,c("project_code","chromosome","chromosome_strand","mutation_type","consequence_type","sequencing_strategy","gene_affected","transcript_affected","icgc_donor_id"),with=FALSE],as.factor),with=FALSE]
+
+	# Save all mutations as cache
+	saveRDS(mutations,"data/simple.somatic.mutations.aggregated.rds",compress = FALSE)
+	
+	# Save single base coding substitutions
+	single.base.coding.substitutions <- mutations[mutation_type=="single base substitution" & consequence_type %in% c("missense_variant", "synonymous_variant", "frameshift_variant","disruptive_inframe_deletion","disruptive_inframe_insertion","inframe_deletion","inframe_insertion","start_lost","stop_lost","stop_gained")]
+	#4,649,834
+
+	# Save single base, coding mutations
+	saveRDS(single.base.coding.substitutions,"data/single.base.coding.substitutions.rds",compress = FALSE)
 }
 
 single.base.coding.substitutions[,.N,by=project_code]
