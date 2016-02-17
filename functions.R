9d0e1d1c24a81fb52b51b3e3357f427bb1ff0888
daniel-wells <daniel-wells@users.noreply.github.com>
2016-02-17 16:17:22 +0000

    correct file name / path for saving archive version
    previously failed to save new file as path was
    “archive/2016-02-17.16:10./results/dNdS.tsv”  (“/results” should not be
    there)

--- functions.R
+++ functions.R
@@ -3,7 +3,7 @@ archive.file <- function(filename){
 	if (file.exists(filename)){
 		print(paste("Archiving old file:",filename))
 	if (file.exists("archive")==FALSE){dir.create("archive", showWarnings = TRUE)}
-	new.filename <- paste("archive/",format(file.mtime(filename),"%Y-%m-%d.%H-%M-%S."),filename,sep="")	
+	new.filename <- paste("archive/",format(file.mtime(filename),"%Y-%m-%d.%H-%M-%S."),basename(filename),sep="")	
 	file.rename(filename, new.filename)
 	}else{
 		print(paste("No file to archive:",filename))
